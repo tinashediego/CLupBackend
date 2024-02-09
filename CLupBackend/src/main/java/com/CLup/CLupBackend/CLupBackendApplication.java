219d682ea282726cf12c464f170ca4777c086fe0
tinashediego <tinashediego@gmail.com>
2024-02-09 17:00:31 +0200

    seeting up springboot enevironment and necessary dependencies

---
+++ CLupBackend/src/main/java/com/CLup/CLupBackend/CLupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.CLup.CLupBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CLupBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CLupBackendApplication.class, args);
+	}
+
+}
---
+++ CLupBackend/src/test/java/com/CLup/CLupBackend/CLupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CLup.CLupBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CLupBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
